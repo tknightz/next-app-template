061c37e316c1813664c21657a8ea04da3f4c14ed
tknightz <hatuyen2699@gmail.com>
2024-08-07 23:07:36 +0700

    feat: init template

---
+++ README.md
@@ -0,0 +1,5 @@
+## My Next.js app template
+
+- App router
+- Biome for linting and formating
+- Bun for package manager and runtime
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div className="prose">
+				<h1 className="text-center">Hello, world!</h1>
+			</div>
+		</main>
+	);
+}
